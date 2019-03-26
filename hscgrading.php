78e499cb677c362dd56264ebf0fd738b439505e6
Francis Mukobi <francismukobi@gmail.com>
2019-03-27 01:08:59 +0300

    Update hscgrading.php

--- hscgrading.php
+++ hscgrading.php
@@ -10,7 +10,105 @@
     if ($conn->connect_error) {
         die("Connection failed: " . $conn->connect_error);
     }
+    //Select posts from  initiating page
+    $theclass = $_POST['theclass'];
+    $stream = $_POST['stream'];
+    $theyear = $_POST['theyear'];
 
+    //Select students to work on
+    $sql = "SELECT student FROM students WHERE theclass='$theclass' AND stream='$stream' 
+        AND theyear='$theyear'";
+    $result = mysqli_query($conn, $sql);
+
+    //Get number of students in stream
+    $num_of_students = $result->rowCount();
+   
+    //Get array of students
+    $student[] = mysqli_fetch_array($result);
+
+    //For each student in the array
+    foreach ($student) :
+
+    //Get the subjects he offers
+   
+$sqlpapers = "SELECT subject, paper FROM marks WHERE student = $student AND theclass = $theclass AND  stream = $stream AND theyear = $theyear";
+        
+        $subjectpapers = $conn->query($sqlpapers);
+        $num_of_papers = mysqli_num_rows($subjectpapers);
+        $thissubjectpaper[] = mysqli_fetch_array($subjectpapers);
+
+             if ($num_of_papers == 2) {
+               foreach ($thissubjectpaper as $sp) : 
+                   //Get marks for each paper
+                   $subject_mark = "SELECT mark1 FROM marks WHERE  student = $student AND subject= $thissubjectpaper AND theclass = $theclass AND  stream = $stream ";
+                   $result_mark = mysqli_query($conn, $subject_mark);
+                    $marks = mysqli_fetch_row($result_mark));
+                    
+                   //Get aggregate per paper
+                  // foreach ($studentmarks as $sf):
+                         
+                        // $marks = $sp['mark1'];
+
+                         if($marks>=80 && $marks<=100){
+                             $aggregate = 1;
+                             
+                         }else if($marks>=75 && $marks<=79){
+                             $aggregate = 2;
+                          
+                         }else if($marks>=70 && $marks<=74){
+                             $aggregate = 3;
+                          
+                         }else if($marks>=65 && $marks<=69){
+                             $aggregate = 4;
+                         
+                         }else if($marks>=60 && $marks<=64){
+                             $aggregate = 5;
+                          
+                         }else if($marks>=50 && $marks<=59){
+                          $aggregate = 6;
+                          
+                         }else if($marks>=45 && $marks<=49){
+                          $aggregate = 7;
+                          
+                         }else if($marks>=35 && $marks<=44){
+                          $aggregate = 8;
+                         
+                         }else if($marks>=0 && $marks<=34){
+                          $aggregate = 9;
+                        
+                         }else{
+                          //Didn't sit for the paper
+                          $aggregate = 9;
+
+                      }
+              //concatnate aggregates 
+              $agg .= $aggregate;
+            
+              //convert concatnated aggregate number into string for comparison purposes with
+              $aggr = strval($agg);
+                    
+                $sql5 = "SELECT CONCAT(paperone, papertwo) FROM twopapergrades";
+                $result = mysqli_query($conn, $sql5);
+                $preset_joined_aggregate[] = mysqli_fetch_array($result));
+                foreach ($preset_joined_aggregates) :
+                    //Compare $students_joined_aggregates with $preset_joined_aggregate
+                    if (strcmp($students_joined_aggregates, $preset_joined_aggregate) == 0) {
+                        //get grade for subject
+                        $sql6 = "SELECT grade FROM twopapergrade WHERE $students_joined_aggregates[] = $preset_joined_aggregates[]; ";
+                        $result = mysqli_query($conn, $sql6);
+                        $grade = mysqli_fetch_row($result));
+                    } else {
+                        echo 'Grade not found in system.';
+                    }
+                endforeach
+             }else{
+              
+              
+             }        
+            endforeach
+                   
+    endforeach
+-------------------------------------
     //get number of students
     $sql = "SELECT student FROM students WHERE theclass = $theclass AND  stream = $stream AND term = $term AND theyear = $theyear";
     $result = mysqli_query($conn, $sql);
@@ -64,34 +162,4 @@
                 endforeach
             } else { }
         endforeach
-        //----------------------------------------------
-
-        if ($num_of_papers == 2) {
-            $sql = "SELECT CONCAT(paperone, papertwo, grade) FROM twopapergrades";
-            $result = $conn->query($sql);
-
-            if ($result->num_rows > 0) {
-                // output data of each row
-                while ($row = $result->fetch_assoc()) {
-                    echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
-                    //compare set grades with grades attained by student in each subject
-                }
-            } else {
-                echo "0 results";
-            }
-        } else if ($num_of_papers == 3) {
-            $sql = "SELECT CONCAT(paperone,papertwo,paperthree,grade) FROM threepapergrades";
-            $result = $conn->query($s
-
-            if ($resul t->num_rows > 0)
-            // output data     of each
-            while ($row = $result->fetch_assoc()) {
-                echo "id: " . $row["id"] . " - Name: " . $row["firstname"] . " " . $row["lastname"] . "<br>";
-                //compare set grades with grades attained by studen        t in eac 
-            } else
-            echo "0 results";
-        }
-
-
-        $conn->close()
-        ?>
+       
